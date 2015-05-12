c2c519150e9f72076dbbcb4decb96bca0c677761
Tomas Korcak <korczis@gmail.com>
2015-05-12 23:33:37 +0200

    Load points

--- examples/viz.js
+++ examples/viz.js
@@ -7,13 +7,13 @@ var user = 'tomas.korcak@gooddata.com',
 //    attr1 = 'oppclose.aam81lMifn6q',
 //    attr2 = 'label.opp_owner.id.name';
 
-var projectId = 'ke0kk4gso44wykymy35h41vb7t0sdsiv'
-    metric = 'amyDTB56dJ5p',
-    attr1 = 'Crime.date.mmddyyyy',
-    attr2 = 'label.Time.Time',
-    attr3 = 'label.Incident.Category',
-    attr4 = 'label.Incident.GeoLatitude',
-    attr5 = 'label.Incident.GeoLongitude';
+var projectId = 'rq3enqarynvkt7q11u0stev65qdwpow8'
+var metric = 'akaFDPTufOga',
+    attr1 = 'label.incidentdata.category',
+    attr2 = 'incidenttime.date.mmddyyyy',
+    attr3 = 'label.locations.xy',
+    attr4 = 'label.locations.neighbourhood';
+
 
 var elements = [
     attr1,
@@ -38,6 +38,9 @@ var projectId = 'rq3enqarynvkt7q11u0stev65qdwpow8',
     attr3 = 'label.locations.xy',
     attr4 = 'label.locations.neighbourhood';
 
+// Threejs layer
+var layer = null;
+
 // For calculating tick time
 var lastCalledTime = Date.now();
 
@@ -46,17 +49,32 @@ var stats = null;
 
 var options = {
     color: "#ff0000",
+    startColor: "#ff0000",
+    stopColor: "#ff0000",
     blending: DEFAULT_BLENDING
 };
 
 // Initialize map
-(function() {
+(function () {
+    // http://graphics.stanford.edu/~seander/bithacks.html
+    // http://bocoup.com/weblog/find-the-closest-power-of-2-with-javascript/
+    function nearestPow2(aSize) {
+        return Math.pow(2, Math.round(Math.log(aSize) / Math.log(2)));
+    }
+
+    function convertColor(color) {
+        var colorObject = new THREE.Color(color);
+        return colorObject.toArray().map(function (c) {
+            return parseInt(c * 255);
+        });
+    }
+
     function generateSprite(size) {
         var canvas = document.createElement('canvas'),
             context = canvas.getContext('2d'),
             gradient;
 
-            size = size || 128;
+        size = nearestPow2(size || 128);
 
         canvas.width = size;
         canvas.height = size;
@@ -65,11 +83,8 @@ var options = {
             canvas.width / 2, canvas.height / 2, canvas.width / 2
         );
 
-        var colorObject = new THREE.Color(options.color);
-        var color = colorObject.toArray().map(function(c) { return parseInt(c * 255); });
-
-        gradient.addColorStop(1.0, 'rgba(' + color.join() + ',0)');
-        gradient.addColorStop(0.0, 'rgba(' + color.join() + ',1)');
+        gradient.addColorStop(1.0, 'rgba(' + convertColor(options.color).join() + ',0)');
+        gradient.addColorStop(0.0, 'rgba(' + convertColor(options.color).join() + ',1)');
         context.fillStyle = gradient;
         context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -79,7 +94,7 @@ var options = {
     /**
      * One tick
      */
-    var tick = function() {
+    var tick = function () {
         // Begin of stats loop
         stats.begin();
 
@@ -111,9 +126,70 @@ var options = {
         stats.domElement.style.left = '0px';
         stats.domElement.style.top = '0px';
 
-        document.body.appendChild( stats.domElement );
+        document.body.appendChild(stats.domElement);
 
         requestAnimationFrame(tick);
+    };
+
+    function initLayer(layer, dataResult) {
+        var geometry = new THREE.Geometry(),
+            texture = new THREE.Texture(generateSprite()),
+            material, particles;
+
+        // Make sure we work with array
+        dataResult = dataResult || [];
+
+        for (var i = 0; i < dataResult.rawData.length; i++) {
+            var entry = dataResult.rawData[i];
+            var coords = entry[2].split(';');
+
+            var lat = parseFloat(coords[0]), lng = parseFloat(coords[1]);
+            var location = new google.maps.LatLng(lat, lng),
+                vertex = layer.fromLatLngToVertex(location);
+            geometry.vertices.push(vertex);
+        }
+
+        texture.needsUpdate = true;
+        material = new THREE.PointCloudMaterial({
+            size: 128,
+            map: texture,
+            opacity: 0.3,
+            blending: BLENDING_TYPES[DEFAULT_BLENDING],
+            depthTest: true,
+            depthWrite: true,
+            transparent: true
+        });
+        particles = new THREE.PointCloud(geometry, material);
+        layer.add(particles);
+
+        var gui = new dat.GUI();
+
+        function update() {
+            material.map = new THREE.Texture(generateSprite(material.size));
+            material.map.needsUpdate = true;
+            layer.render();
+        }
+
+        // Initialize loop
+        var points = gui.addFolder('Points');
+
+        points.add(material, 'size', 2, 1024).onChange(update);
+        points.add(material, 'opacity', 0.1, 1).onChange(update);
+        points.addColor(options, 'color').onChange(update);
+        // points.addColor(options, 'startColor').onChange(update);
+        // points.addColor(options, 'stopColor').onChange(update);
+        points.add(options, 'blending', Object.keys(BLENDING_TYPES)).onChange(function () {
+            material.blending = BLENDING_TYPES[options.blending];
+            material.needsUpdate = true;
+            layer.render();
+        });
+
+        points.open();
+
+        // And finally initLoop
+        initLoop();
+
+        return layer;
     };
 
     /**
@@ -121,7 +197,7 @@ var options = {
      */
     function initialize() {
         var mapOptions = {
-            zoom: 8,
+            zoom: 13,
             center: new google.maps.LatLng(37.773972, -122.431297),
             mapTypeControlOptions: {
                 mapTypeIds: []
@@ -142,74 +218,32 @@ var options = {
         var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
         // Initialize three.js layer
-        new ThreejsLayer({ map: map }, function(layer){
-            var geometry = new THREE.Geometry(),
-                texture = new THREE.Texture(generateSprite()),
-                material, particles;
+        layer = new ThreejsLayer({map: map}, function (layer) {
 
-            for(var i = 0; i < 100000; i++) {
-                var location = new google.maps.LatLng(37.773972 + Math.random() * 10, -122.431297 + Math.random() * 10),
-                    vertex = layer.fromLatLngToVertex(location);
-                geometry.vertices.push( vertex );
-            }
-
-            texture.needsUpdate = true;
-            material = new THREE.PointCloudMaterial({
-                size: 128,
-                map: texture,
-                opacity: 0.3,
-                blending: BLENDING_TYPES[DEFAULT_BLENDING],
-                depthTest: true,
-                depthWrite:true,
-                transparent: true
-            });
-            particles = new THREE.PointCloud( geometry, material );
-            layer.add(particles);
-
-            var gui = new dat.GUI();
-            function update(){
-                material.map = new THREE.Texture(generateSprite(material.size));
-                material.map.needsUpdate = true;
-                layer.render();
-            }
-
-            // Initialize loop
-            var points = gui.addFolder('Points');
-
-            points.add(material, 'size', 2, 1024).onChange(update);
-            points.add(material, 'opacity', 0.1, 1).onChange(update);
-            points.addColor(options, 'color').onChange(update);
-            points.add(options, 'blending', Object.keys(BLENDING_TYPES)).onChange(function() {
-                material.blending = BLENDING_TYPES[options.blending];
-                material.needsUpdate = true;
-                layer.render();
-            });
-
-            points.open();
-
-            // And finally initLoop
-            initLoop();
         });
     }
 
     // Set listener which will trigger initialization after everything is loaded
     google.maps.event.addDomListener(window, 'load', initialize);
+
+    // Login
+    gooddata.user.login(user, passwd).then(function () {
+        // Ask for data for the given metric and attributes from the GoodSales project
+        var params = {
+            filters: [{
+                "incidenttime.aci81lMifn6q": {"id": 8059}
+            }]
+        };
+
+        // TODO: Get the data here
+        gooddata.execution.getData(projectId, elements).then(function(dataResult) {
+            // Yay, data arrived
+
+            initLayer(layer, dataResult).render();
+
+            console.log(dataResult);
+        });
+    });
 }());
 
-// Login
-gooddata.user.login(user, passwd).then(function() {
-    // Ask for data for the given metric and attributes from the GoodSales project
-    var params = {
-        filters: [{
-            "incidenttime.aci81lMifn6q": {"id": 8059 }
-        }]
-    };
-
-    // TODO: Get the data here
-    //gooddata.execution.getData(projectId, elements, params).then(function(dataResult) {
-    //    // Yay, data arrived
-    //
-    //    console.log(dataResult);
-    //});
-});
 
